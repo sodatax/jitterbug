d8e485716e6e1a341a07fa986589a19eefa89f60
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-01-27 19:01:01 +0000

    rendered bug in middle of screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
+// Butano
 #include <bn_core.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_sprite_ptr.h>
+
+#include "bn_sprite_items_bug.h"
 
 int main(){
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(31,11,11));
+    bn::backdrop::set_color(bn::color(31, 11, 11));
 
-    while(true){
-        
+    bn::sprite_ptr green_beetle = bn::sprite_items::bug.create_sprite();
+
+    while (true)
+    {
         bn::core::update();
     }
 }
